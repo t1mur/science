61979d88446a21bfbcca4fe3a06cdc2dd624a9eb
t1mur <timur11@gmail.com>
2012-10-17 12:23:55 -0400

    first commit

---
+++ constants.py
@@ -0,0 +1,30 @@
+# Here are some comments
+
+import scipy as sp
+
+from scipy import constants
+
+print(constants.hbar)
+
+class Na:
+	d2omega=2*constants.pi*508.848716e12
+	d2lambda=589.158326e-9
+	d2gamma=2*constants.pi*9.795e6
+	hyperfine_splitting=2*constants.pi*1.771626e9
+	nuclear_spin=1.5
+	
+class Li:
+	d2omega=2*constants.pi*446.7896e12
+	d2lambda=670.992e-9
+	d2gamma=2*constants.pi*5.8724e6
+	hyperfine_splitting=2*constants.pi*228.2e6
+	nuclear_spin=1
+#To do: Breit Rabi stuff
+		
+		
+class PICamera
+	pixelsize=1e-6
+	
+class PixelFlyCamera
+	pixelwidth=6.4e-6		
+print(Na.d2omega)
